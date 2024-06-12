0118343c93e10cdfa92794bf1a466b796b0bf10c
Lukas Mahler <lukas.mahler@student.tuwien.ac.at>
2024-06-12 18:57:56 +0200

    fix: drop row_nr for train test splitting

--- src/group_33/util.py
+++ src/group_33/util.py
@@ -4,7 +4,7 @@ from polars.lazyframe import LazyFrame
 
 def train_test_split(df: LazyFrame, fraction_train: float = 0.8) -> tuple[LazyFrame, LazyFrame]:
     df = df.with_columns(pl.all().shuffle(seed=1)).with_row_count()
-    df_train = df.filter(pl.col("row_nr") < pl.col("row_nr").max() * fraction_train)
-    df_test = df.filter(pl.col("row_nr") >= pl.col("row_nr").max() * fraction_train)
+    df_train = df.filter(pl.col("row_nr") < pl.col("row_nr").max() * fraction_train).drop("row_nr")
+    df_test = df.filter(pl.col("row_nr") >= pl.col("row_nr").max() * fraction_train).drop("row_nr")
 
     return df_train, df_test
