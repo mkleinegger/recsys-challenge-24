a9ca9e626fd147c190a9a53e93372abdecab6aa4
Lukas Mahler <lukas.mahler@student.tuwien.ac.at>
2024-06-12 17:41:14 +0200

    feat: add splitting function and ranking calculation

---
+++ src/group_33/test.py
@@ -0,0 +1,35 @@
+import polars as pl
+from polars.lazyframe import LazyFrame
+
+
+def group_scores(prediction_file: str, index_file: str):
+    """Combine the prediction file (contains only a single column of scores) with the previously generated index file, which contains a column that specifies to which row of the original testfile a score belongs and the impression id.
+    The resulting LazyFrame of the concatenation is then grouped by the impression_id, where all scores of the same impression id are concatenated, resulting in the same layout as the original test file with the same order of rows."""
+    grouped_scores = (
+        pl.concat([
+            pl.scan_csv(prediction_file, has_header=False, new_columns=["score"]),
+            pl.scan_parquet(index_file)
+            ], how = "horizontal")
+        .group_by(["index", "impression_id"])
+        .agg(pl.col("score"))
+        .sort("index")
+        .drop("index")
+    )
+
+    return grouped_scores
+
+
+def calculate_rankings(grouped_scores: pl.LazyFrame):
+    """Calculate the rankings for the given score column and return the resulting LazyFrame which
+    has the correct schema for a handin at the RecSys challenge (columns: impression_id, ranking)."""
+    rankings = (grouped_scores
+        .with_columns(
+            pl.col("score")
+            .map_elements(lambda scores: [rank + 1 for rank, index in sorted(enumerate(np.argsort(np.array(scores))[::-1]), key=lambda el: el[1])])
+            .alias("ranking"))
+        .drop("score")
+        .with_columns(pl.col("ranking").map_elements(lambda el: str(el.to_list()).replace(" ", "")))
+        .select("impression_id", "ranking")
+    )
+
+    return rankings
---
+++ src/group_33/util.py
@@ -0,0 +1,10 @@
+import polars as pl
+from polars.lazyframe import LazyFrame
+
+
+def train_test_split(df: LazyFrame, fraction_train: float = 0.8) -> tuple[LazyFrame, LazyFrame]:
+    df = df.with_columns(pl.all().shuffle(seed=1)).with_row_count()
+    df_train = df.filter(pl.col("row_nr") < pl.col("row_nr").max() * fraction_train)
+    df_test = df.filter(pl.col("row_nr") >= pl.col("row_nr").max() * fraction_train)
+
+    return df_train, df_test
