70c708f83314a10d15a68fdf531b9c924981d00e
e12024016 <tobias.grantner@tuwien.ac.at>
2024-07-07 22:56:03 +0200

    Guarantee ordering of predictions for DKN

--- src/group_33/test.py
+++ src/group_33/test.py
@@ -2,7 +2,7 @@ import polars as pl
 import numpy as np
 
 
-def calculate_rankings(scores_file: str, indexed_behaviors_file: str):
+def calculate_rankings(indexed_behaviors_file: str, scores_file: str):
     """
     Combine the prediction file (contains only a single column of scores) with the previously generated index file, which contains a column that specifies to which row of the original testfile a score belongs and the impression id.
     The resulting LazyFrame of the concatenation is then grouped by the impression_id, where all scores of the same impression id are concatenated, resulting in the same layout as the original test file with the same order of rows.
@@ -15,14 +15,17 @@ def calculate_rankings(scores_file: str, indexed_behaviors_file: str):
             pl.scan_csv(scores_file, has_header=False, new_columns=["score"]),
             pl.scan_parquet(indexed_behaviors_file),
         ], how="horizontal")
-        .group_by("impression_id")
-        .agg(pl.col("index"), pl.col("score"))
+        .group_by("index", "impression_id")
+        .agg(pl.col("article_index"), pl.col("score"))
         .with_columns(
-            pl.struct(["index", "score"])
-                .map_elements(lambda x: (np.array(x["index"])[np.argsort(x["score"])[::-1]] + 1).tolist())
+            pl.struct(["article_index", "score"])
+                .map_elements(lambda x: (np.array(x["article_index"])[np.argsort(x["score"])[::-1]] + 1).tolist())
                 .map_elements(lambda el: str(el.to_list()).replace(" ", ""))
                 .alias("ranking")
         )
+        .drop("article_index", "score")
+        .collect(streaming=True)
+        .sort("index")
         .select("impression_id", "ranking")
     )
 
