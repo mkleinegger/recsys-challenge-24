2583f363ffe1dc48a90076669903c3b453939618
Josef Taha <josef.taha@tuwien.ac.at>
2024-07-08 23:24:34 +0200

    changed nrms notebook name

--- run.sh
+++ run.sh
@@ -28,7 +28,7 @@ fi
 if [ "$1" = "lstur" ]; then
     NB_PATH="./notebooks/lstur.ipynb"
 elif [ "$1" = "nrms" ]; then
-    NB_PATH="./notebooks/nrms_ebnerd.ipynb"
+    NB_PATH="./notebooks/nrms.ipynb"
 else
     NB_PATH="./notebooks/dkn.ipynb"
 fi
